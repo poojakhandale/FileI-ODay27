28a468e2bdad247daf86515f6055806b62dcbc67
poojakhandale <poojakhandale2000@gmail.com>
2023-03-16 20:51:58 +0530

    Greeting massage

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more informatinon
+namespace FileIODemo
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Address Book File IO");
+    }
+}
+}
