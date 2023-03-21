17077b55a708299bc3e571d47b4511055bebab90
poojakhandale <poojakhandale2000@gmail.com>
2023-03-21 23:13:57 +0530

    new contact

--- Contacts.cs
+++ Contacts.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace FileIODemo
 {
-   
 
 
-    
-        public class Contacts
-        {
-            public string firstName { get; set; }
-            public string lastName { get; set; }
-            public string country { get; set; }
-            public string phoneNumber { get; set; }
-            public string email { get; set; }
+
+
+    public class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string country { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
 
         public Contacts(string firstName, string lastName, string country, string phoneNumber, string email)
         {
@@ -25,9 +25,9 @@ namespace FileIODemo
             this.country = country;
             this.phoneNumber = phoneNumber;
             this.email = email;
-        }
+
         }
 
     }
-
+}
 
